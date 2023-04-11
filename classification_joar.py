54cdc71b92d7fc065b9be2bae863d4e05eb19634
Joar Lönngren <joarlonngren@gmail.com>
2023-04-11 14:18:09 +0200

    Uppdated with filename in headder in csv

--- classification_joar.py
+++ classification_joar.py
@@ -7,8 +7,8 @@ import pandas as pd
 
 #Read Data
 PATH = '//NAS24/solo/remote/data/L2/tds_wf_e'
-YEAR = '2023'
-MONTH = '03' #need to be in format XX, not just X
+YEAR = '2020'
+MONTH = '03'                #need to be in format XX, not just X
 
 FILE_PATH = f"{PATH}/{YEAR}/{MONTH}"
 model_FILE_PATH = 'C:/Users/joarl/OneDrive/Dokument/Skola/Kand/Kod_Kvammen/AndreasKvammen-ML_dust_detection-9c0d0de/CDF file classification/model_run_GitHub'
@@ -20,27 +20,30 @@ DAY = 1 #day-counter
 
 for file in os.listdir(FILE_PATH):    #iterate folder
     if 'tswf' in file:                #only use tswf-data
-        if DAY%2:                     #take only odd days
+
+        if DAY%2:                     #take only half of the days
             current_file_path = f"{FILE_PATH}/{file}"
             WAVEFORM, SAMPLING_RATE, EPOCHS = get_data(current_file_path, to_extract='WAVEFORM_DATA_VOLTAGE', want_DOWNLINK_INFO=False)
             flags = []
+
             for EPOCH in EPOCHS:
                 times, data = gen_timeseries(WAVEFORM, SAMPLING_RATE, EPOCH)
                 times_processed, DATA_processed = process_data(times, data, compression=4)
                 DATA_processed = np.array(DATA_processed).transpose()
                 DATA_processed = np.reshape(DATA_processed, (-1, 4096, 3))
                 flags.append(model.predict(DATA_processed))
+            
             FLAGGED_EPOCHS[file] = flags
             probabilities = FLAGGED_EPOCHS[file]
             prob = [item[0][1] for item in probabilities] #save prob dust
 
-            df_day = pd.DataFrame({f'{DAY}': prob})
+            df_day = pd.DataFrame({f'{file}': prob})
             df = pd.concat([df, df_day], axis=1)
-            DAY += 1
 
-            print(DAY)
+            print(DAY) #just to see it uppdateing
+            DAY += 1
         else:
             DAY +=1
 
-FILENAME = f"Predicitons_{YEAR}_{MONTH}.csv"
+FILENAME = f"predictions_{YEAR}_{MONTH}.csv"
 df.to_csv(FILENAME, index=False)
