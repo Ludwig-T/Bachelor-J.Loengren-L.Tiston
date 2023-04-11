c1b64e5597e53c73e5811b5cef71a9c836351a6c
Joar Lönngren <joarlonngren@gmail.com>
2023-04-11 14:19:59 +0200

    Initial commit

---
+++ SOLO_orbit/SOLO_orbit_plot.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+PATH = 'SOLO_orbit_HCI.txt'
+
+
+df = pd.read_csv(PATH, sep=' ')
+# combine the year, month, and day columns into a single datetime column
+df['datetime'] = pd.to_datetime(df[['year', 'month', 'day']])
+
+# group the data by date and calculate the mean of R[AU] for each date
+daily_mean = df.groupby('datetime')['R[AU]'].mean()
+
+plt.plot(daily_mean.index, daily_mean.values)
+plt.title('Mean R[AU] per day')
+plt.xlabel('Date')
+plt.ylabel('Mean R[AU]')
+plt.show()
