81895d3e87e0c10f8f125fa850b7acd1da29fd27
Ludwig Tiston <ludwigtiston@gmail.com>
2023-04-21 09:47:23 +0200

    Better storage as dicts in pkl format

--- label_data.py
+++ label_data.py
@@ -10,7 +10,7 @@ folderpath_pred = 'C:/Data/predictions'
 
 #Threshold for prediction
 threshold = 0.9
-dic = {}
+main_dic = {}
 
 for filename in os.listdir(folderpath_pred):
     filepath_pred = os.path.join(folderpath_pred, filename)
@@ -18,24 +18,40 @@ for filename in os.listdir(folderpath_pred):
     # checking if it is a file
     if os.path.isfile(filepath_pred):
         df = pd.read_csv(filepath_pred)
-        epochs_info = []
+
         for file in df.columns:
             filepath = os.path.join(folderpath_data, file)
-            WAVEFORM, SAMPLING_RATE, EPOCHS = get_data(filepath, to_extract='WAVEFORM_DATA_VOLTAGE', want_DOWNLINK_INFO=False)
-            QUALITY, _, _ = get_data(filepath, to_extract='QUALITY_FACT')
+            WAVEFORM, SAMPLING_RATE, EPOCHS = get_data(filepath, to_extract='WAVEFORM_DATA_VOLTAGE',\
+                                                        want_DOWNLINK_INFO=False)
+            
+            data_dict = {
+            "epoch": [],
+            "prediction": [],
+            "amplitude": [],
+            "wavelenght": [],
+            "waveQ": []
+            }
+            
             for EPOCH, prediction in enumerate(df[file]):
                 if prediction > threshold:
                     X, Y = gen_timeseries(WAVEFORM, SAMPLING_RATE, EPOCH)
                     amplitude = get_amp(X, Y, to_plot=False)
                     amplitude = [abs(Mx - mn) for mn, Mx in amplitude]
-                    wavelenght, waveQ = get_wavelenght(X, Y, to_plot=False, title=str(QUALITY[EPOCH]), thresh_above=0.04, thresh_below=0.001)
-                    epochs_info.append([EPOCH, QUALITY[EPOCH], prediction, amplitude, wavelenght, waveQ])
-            dic[file] = epochs_info
+                    wavelenght, waveQ = get_wavelenght(X, Y, to_plot=False,\
+                        thresh_below=[0.01, 0.01, 0.01], thresh_above=[0.02, 0.02, 0.02])
+                    
+                    data_dict['epoch'].append(EPOCH)
+                    data_dict['amplitude'].append(amplitude)
+                    data_dict['prediction'].append(prediction)
+                    data_dict['wavelength'].append(wavelenght)
+                    data_dict['waveQ'].append(waveQ)
+
+            main_dic[file] = data_dict
             print(str(file) + ' complete')
 
-new_df = pd.DataFrame.from_dict(dic, orient='index')
+new_df = pd.DataFrame.from_dict(main_dic, orient='index')
 new_df = new_df.transpose()
-new_df.to_csv('Labels.csv', index=False, header=True)
+new_df.to_pickle('Labels.pkl')
   
        
        
