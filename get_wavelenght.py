b34cd660f5e772b82a2b805cdc0e031809b866a7
Ludwig Tiston <ludwigtiston@gmail.com>
2023-04-21 09:45:54 +0200

    Enabled possibility to plot with waveQ as title

--- get_wavelenght.py
+++ get_wavelenght.py
@@ -3,7 +3,8 @@ import scipy.signal as scisig
 import numpy as np
 import matplotlib.pyplot as plt
 
-def get_impact_time(times, DATA, reverse=False, thresh_above=0.05, thresh_below = 0.005):
+def get_impact_time(times, DATA, reverse=False, \
+        THRESH_HIGH=[0.05, 0.05, 0.05], THRESH_LOW=[0.005, 0.005, 0.005]):
     '''Input is timeseries of antennas. Output is each antennas wavelenghts start and end time.
     If reverse is True then it returns the end time of impact'''
     
@@ -17,7 +18,7 @@ def get_impact_time(times, DATA, reverse=False, thresh_above=0.05, thresh_below 
     if reverse:
         times = np.flip(times)
 
-    for data in DATA:
+    for thresh_above, thresh_below, data in zip(THRESH_HIGH, THRESH_LOW, DATA):
         #Process data:
         #Remove bias
         F = 21
@@ -54,19 +55,53 @@ def get_impact_time(times, DATA, reverse=False, thresh_above=0.05, thresh_below 
     return impact_time_per_antenna
                 
         
-def get_wavelenght(times, DATA, thresh_below=0.005, thresh_above=0.05, to_plot=False, title="Waveform"):
+def get_wavelenght(times, DATA, \
+    thresh_below, thresh_above, to_plot=False, title="Quality"):
     '''Returns the wavelenght (time) and if the esstimation is good (1=good, 0=uncertain) for an epochs timeseries.
-    Can plot if desired.'''    
-    time_start_per_antenna = get_impact_time(times, DATA, thresh_below=thresh_below, thresh_above=thresh_above)
-    time_end_per_antenna = get_impact_time(times, DATA, thresh_below=thresh_below, thresh_above=thresh_above, reverse=True)
+    Can plot if desired.'''
+    
+    max_indices = [np.argmax(np.abs(subarray)) for subarray in DATA]
+    t_lim10 = times[len(times)//10]
+    good_est = 1
+    
+    done = False
+    while not done:  
+        time_start_per_antenna = get_impact_time(times, DATA, THRESH_HIGH=thresh_above, THRESH_LOW=thresh_below)
+        time_end_per_antenna = get_impact_time(times, DATA, THRESH_HIGH=thresh_above, THRESH_LOW=thresh_below,\
+            reverse=True)
+
+        checks = 0
+        for i, (t_start, t_end) in enumerate(zip(time_start_per_antenna, time_end_per_antenna)):
+            time_max = times[max_indices[i]]
+            #If t_start is too far from the max peak or the wavelenght is too long, it is probably detecting noise.
+            if abs(time_max - t_start) > t_lim10/2 or abs(t_end - t_start > 10):
+                #Increase parameter
+                thresh_above[i] *= 1.8
+
+                #Couldn't find good wavelenght
+                if thresh_above[i] > 1:
+                    good_est = 0
+                    done = True
+                    break
+            else:
+                checks += 1
+
+        #All three antennas should be good.
+        if checks == 3:
+            break
+    
+    for t in time_end_per_antenna:
+        #Wavelenghts shouldn't differ that much.
+        if t - np.mean(time_start_per_antenna) > 10:
+            good_est = 0
+
     if to_plot:
+        if title == 'Quality':
+            title = good_est
         fig, axes = tripple_plot(times, DATA, to_show=False, title=title)
         for i, ax in enumerate(axes):
             ax.axvline(time_start_per_antenna[i], linestyle='--', color='g')
             ax.axvline(time_end_per_antenna[i], linestyle='--', color='r')
         plt.show()
-    good_est = 1    
-    for t in time_end_per_antenna:
-        if t - np.mean(time_start_per_antenna) > 10:
-            good_est = 0
+    
     return [t_end-t0 for t0, t_end in zip(time_start_per_antenna, time_end_per_antenna)], good_est
