36475fde7fc42b7065ff1089af22da9b1cd02bd6
Ludwig Tiston <ludwigtiston@gmail.com>
2023-04-25 11:35:27 +0200

    Add adaptable noise

--- data_extension.py
+++ data_extension.py
@@ -1,12 +1,24 @@
 import numpy as np
 
-def extend_data(times, data):
+def extend_data(times, data, adaptable_noise=True):
+    '''Extends data with noise'''
     np.random.seed(42)
-    sigma = 0.0005 #parameter that can be tweaked, 
-
-    #generate noise
-    noise1 = np.random.randn(3, 8192)*sigma
-    noise2 = np.random.randn(3, 8192)*sigma 
+    #Noise depending on data variance
+    if adaptable_noise:
+        sigma_antennas = []
+        for antenna in data:
+            sigma = np.std(antenna)     #Standard deviation of each antenna
+            sigma_antennas.append(sigma)
+            
+        sigmas = np.array(sigma_antennas)
+        noise1 = np.random.randn(3, 8192)*sigmas.reshape(3,1)
+        noise2 = np.random.randn(3, 8192)*sigmas.reshape(3,1)
+        
+    else:
+        sigma = 0.0005 #parameter that can be tweaked, 
+        #generate noise
+        noise1 = np.random.randn(3, 8192)*sigma
+        noise2 = np.random.randn(3, 8192)*sigma 
 
     #add noise to data
     data_noise1 = np.hstack((noise1, data))      
