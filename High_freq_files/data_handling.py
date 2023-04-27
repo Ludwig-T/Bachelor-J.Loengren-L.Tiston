ef1358d96330ad8b38f944bc7e461cb9aa12c51b
Joar Lönngren <joarlonngren@gmail.com>
2023-04-27 13:24:13 +0200

    uppdates with nicer plots

--- High_freq_files/data_handling.py
+++ High_freq_files/data_handling.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:data_handling.py
 import cdflib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -60,55 +59,9 @@ def gen_timeseries(WAVEFORM, SAMPLING_RATE, EPOCH):
     start_time = 0
     end_time = data_size*timestep
     times = np.arange(start_time, end_time ,timestep)
-=======
-import cdflib
-import matplotlib.pyplot as plt
-import numpy as np
-
-def tripple_plot(X, Y, to_show=True, title="Electric field (V/m)"):
-    '''Plots 3 graphs on common x-axis'''
-    fig, axes = plt.subplots(3, 1, sharex=True, sharey=True)
-    
-    # Plot data on each subplot
-    if isinstance(Y, np.ndarray):
-        axes[0].plot(X, Y[0, :, 0])
-        axes[1].plot(X, Y[0, :, 1])
-        axes[2].plot(X, Y[0, :, 2])
-    else:
-        axes[0].plot(X, Y[0])
-        axes[1].plot(X, Y[1])
-        axes[2].plot(X, Y[2])
-    
-    fig.add_subplot(111, frameon=False)
-    plt.tick_params(labelcolor='none', which='both', top=False, bottom=False, left=False, right=False)
-    plt.xlabel("Time (ms)")
-    plt.title(title)
-    if to_show:
-        plt.show()
-    else:
-        return fig, axes
-
-def get_data(filepath, to_extract="WAVEFORM_DATA", want_DOWNLINK_INFO=True):
-    '''Extracts waveform data, sampling rate and (if wanted) downlink info from cdf file'''
-    c = cdflib.cdfread.CDF(filepath)
-    WAVEFORM = c[to_extract]
-    SAMPLING_RATE = c['SAMPLING_RATE']
-    if want_DOWNLINK_INFO:
-        FLAGs = [1 if flag[1] == 2 else 0 for flag in c['DOWNLINK_INFO']] #Flags for dust by onboard comp.
-        FLAGGED_EPOCHS = [i for i in range(len(FLAGs)) if FLAGs[i] == 1]       #Indicies for flagged dust
-        return WAVEFORM, SAMPLING_RATE, FLAGGED_EPOCHS
-    else:
-        EPOCHS = [i for i in range(len(SAMPLING_RATE))]
-        return WAVEFORM, SAMPLING_RATE, EPOCHS
-    
-def gen_timeseries(WAVEFORM, SAMPLING_RATE, EPOCH):
-    '''For given epoch, generates waveform timeseries for each antenna based on sampling rate'''
-    Y = WAVEFORM[EPOCH,:,:]
-    Y = [Y[0,:], Y[1,:], Y[2,:]]
-    data_size = Y[0].size
-    timestep = (SAMPLING_RATE[EPOCH])**(-1)*1000 #from Hz to ms
-    start_time = 0
-    end_time = data_size*timestep
-    times = np.arange(start_time, end_time ,timestep)
->>>>>>> 45d7969bd2ddb88384cefb94e300238deb5c3fd6:High_freq_files/data_handling.py
     return times, Y
+
+def smooth(y, box_pts):
+    box = np.ones(box_pts)/box_pts
+    y_smooth = np.convolve(y, box, mode='same')
+    return y_smooth
