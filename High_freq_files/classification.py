e080f6eeb231689287f00fb968bc3c9564caa175
Ludwig Tiston <ludwigtiston@gmail.com>
2023-04-27 15:21:30 +0200

    Unclear change

--- High_freq_files/classification.py
+++ High_freq_files/classification.py
@@ -6,8 +6,8 @@ from pre_processing import process_data
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 #Read Data
-FILE_PATH = "C:/Users/joarl/OneDrive/Skrivbord/anomoly/"
-model_FILE_PATH = 'C:/Users/joarl/OneDrive/Dokument/Skola/Kand/Kod_Kvammen/AndreasKvammen-ML_dust_detection-9c0d0de/CDF file classification/model_run_GitHub'
+FILE_PATH = "C:/Data/anomaly/"
+model_FILE_PATH = "C:\Data\model\model_run_GitHub"
 model = tf.keras.models.load_model(model_FILE_PATH)
 
 file_to_plot = 1
@@ -29,19 +29,19 @@ for root, dirs, files in os.walk(FILE_PATH):    #iterate folders
                 times, data = gen_timeseries(WAVEFORM, SAMPLING_RATE, EPOCH)
                 
                 times_processed, DATA_processed = \
-                    process_data(times, data, SAMPLING_RATE[EPOCH], compression=4, adaptable_noise=True)
+                    process_data(times, data, SAMPLING_RATE[EPOCH], compression=4, adaptable_noise=False)
 
                 if DATA_processed is not None and file_counter == file_to_plot:
                     prediction = model.predict(DATA_processed)[0][1]
                     #flags.append(prediction)
                     if prediction > 0.95:
                         flag_counter += 1
-        """         times_processed2, DATA_processed2 =\
+                    times_processed2, DATA_processed2 =\
                         process_data(times, data, SAMPLING_RATE[EPOCH], compression=4, adaptable_noise=True)
                     tripple_plot(times, data, title=f"Prediction: {prediction:.2f}")
                     tripple_plot(times_processed, DATA_processed, title=f"Prediction: {prediction:.2f}, static noise")
                     prediction = model.predict(DATA_processed2)[0][1]
-                    tripple_plot(times_processed2, DATA_processed2, title=f"Prediction: {prediction:.2f}, adaptable noise")"""  
+                    tripple_plot(times_processed2, DATA_processed2, title=f"Prediction: {prediction:.2f}, adaptable noise")  
             #FLAGGED_EPOCHS[file] = flags
             #file_counter += 1
                 
