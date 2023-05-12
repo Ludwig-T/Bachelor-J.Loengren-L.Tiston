96a51e5c47749725ed3e369a9ba840d248536637
Ludwig Tiston <ludwigtiston@gmail.com>
2023-05-12 09:57:52 +0200

    No longer changes when calling 2nd time

--- High_freq_files/pre_processing.py
+++ High_freq_files/pre_processing.py
@@ -3,10 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from data_extension import extend_data
 
-def process_data(times, DATA, SAMPLING_RATE=1, compression=4, to_plot=False, adaptable_noise=True, to_print=True):
+def process_data(times, DATA, SAMPLING_RATE=1, compression=4,\
+    to_plot=False, adaptable_noise=True, to_print=True, extension='noise'):
     '''Removes bias, filters, compresses and normalizes data. to_print gives error messages.
     Compression = 4 for correct model input. to_plot plots processing steps.'''
     #Compress time
+    new_DATA = DATA.copy()
+    new_times = times
     if SAMPLING_RATE < 300000 and len(times) > 32000:
         if to_print:
             print(f'Anomalous data: Long timeseries ({len(times)} points)')
@@ -18,17 +21,23 @@ def process_data(times, DATA, SAMPLING_RATE=1, compression=4, to_plot=False, ada
             print(f'Anomalous data: Long timeseries ({len(times)} points) and high sampling rate ({SAMPLING_RATE} Hz)')
             print('Increasing compression')
         compression *= 2
-    elif SAMPLING_RATE > 300000 and len(times) < 32000:
+    elif SAMPLING_RATE > 300000 and len(times) < 32000 and extension == 'noise':
         if to_print:
             print(f'Anomalous data: High sampling rate ({SAMPLING_RATE} Hz)')
             print('Extending data')
-        times, DATA = extend_data(times, DATA, adaptable_noise)
-    
-    times_processed = np.linspace(times[0],times[-1],round(len(times)/compression))
+        new_times, new_DATA = extend_data(times, DATA, adaptable_noise)
+    elif SAMPLING_RATE > 300000 and len(times) < 32000 and extension == 'copy':
+        new_times = times*2
+
+        for i in range(len(DATA)):
+            new_DATA[i] = new_DATA[i][::2]
+            new_DATA[i] = np.append(new_DATA[i], new_DATA[i])
+            
+    times_processed = np.linspace(new_times[0],new_times[-1],round(len(new_times)/compression))
     
     #Iterate over all three antennas
     DATA_processed = []
-    for data in DATA:
+    for data in new_DATA:
         #Flatten and calculate median to remove bias
         F = 21
         flattened_data = scisig.medfilt(data, kernel_size=F)
@@ -39,7 +48,7 @@ def process_data(times, DATA, SAMPLING_RATE=1, compression=4, to_plot=False, ada
         filtered_data = scisig.medfilt(data_nobias, kernel_size=7)
         
         #Compress data
-        data_compressed = np.interp(times_processed, times, filtered_data)
+        data_compressed = np.interp(times_processed, new_times, filtered_data)
         
         #Normalize with respect to maximum
         mx = max(abs(data_compressed))
@@ -48,11 +57,11 @@ def process_data(times, DATA, SAMPLING_RATE=1, compression=4, to_plot=False, ada
         DATA_processed.append(data_processed)
         
         if to_plot:
-            plt.plot(times, data_nobias)
+            plt.plot(new_times, data_nobias)
             plt.title('No Bias')
             plt.show()
 
-            plt.plot(times, filtered_data)
+            plt.plot(new_times, filtered_data)
             plt.title('Filtered')
             plt.show()
 
