51423dd02d96d11d08b4dc91741af6e07a9ea84a
Ludwig Tiston <ludwigtiston@gmail.com>
2023-05-12 09:59:03 +0200

    Trash

--- Low_freq_files/read_mat.py
+++ Low_freq_files/read_mat.py
@@ -2,8 +2,14 @@ import scipy.io
 import os
 
 folder_path = 'C:/Data/label_low_freq'
+
+def read_mat(file_path):
+    data = scipy.io.loadmat(file_path)
+    print(len(data['l1'][0][0][0]))
+
 for filename in os.listdir(folder_path):
     file_path = os.path.join(folder_path, filename)
     data = scipy.io.loadmat(file_path)
-    print(len(data['l1'][0][0][0]))
+    print(data['l1'][0][0][4][0][0][0][0][4])
     break
+    
